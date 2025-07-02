390c64d7019740a44f057f4244895cb3957d4239
najoan125 <tiaaa0012@gmail.com>
2025-07-02 21:58:14 +0900

    Fix: add MSVC compatibility for logging functions

--- src/win/logger.c
+++ src/win/logger.c
@@ -1,8 +1,15 @@
+#if !defined(_MSC_VER)
 #define _GNU_SOURCE
+#endif
 
 #include "logger.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+// For MSVC, we need to include this for _vscprintf and vsprintf_s
+#if defined(_MSC_VER)
+#include <stdarg.h>
+#endif
 
 struct {
     LogFunction log;
@@ -14,30 +21,93 @@ void CILog(const char *format, ...) {
     char *msg;
     va_list args;
     va_start(args, format);
-    vasprintf(&msg, format, args);
-    LogPointer.log(msg);
-    free(msg);
+
+#if defined(_MSC_VER)
+    int len = _vscprintf(format, args);
+    if (len < 0) {
+        va_end(args);
+        return;
+    }
+    size_t size = (size_t)len + 1;
+    msg = malloc(size);
+    if (!msg) {
+        va_end(args);
+        return;
+    }
+    vsprintf_s(msg, size, format, args);
+#else
+    if (vasprintf(&msg, format, args) == -1) {
+        msg = NULL; // Ensure msg is NULL on failure
+    }
+#endif
     va_end(args);
+
+    if (msg) {
+        LogPointer.log(msg);
+        free(msg);
+    }
 }
 
 void CIError(const char *format, ...) {
     char *msg;
     va_list args;
     va_start(args, format);
-    vasprintf(&msg, format, args);
-    LogPointer.error(msg);
-    free(msg);
+
+#if defined(_MSC_VER)
+    int len = _vscprintf(format, args);
+    if (len < 0) {
+        va_end(args);
+        return;
+    }
+    size_t size = (size_t)len + 1;
+    msg = malloc(size);
+    if (!msg) {
+        va_end(args);
+        return;
+    }
+    vsprintf_s(msg, size, format, args);
+#else
+    if (vasprintf(&msg, format, args) == -1) {
+        msg = NULL;
+    }
+#endif
     va_end(args);
+
+    if (msg) {
+        LogPointer.error(msg);
+        free(msg);
+    }
 }
 
 void CIDebug(const char *format, ...) {
     char *msg;
     va_list args;
     va_start(args, format);
-    vasprintf(&msg, format, args);
-    LogPointer.debug(msg);
-    free(msg);
+
+#if defined(_MSC_VER)
+    int len = _vscprintf(format, args);
+    if (len < 0) {
+        va_end(args);
+        return;
+    }
+    size_t size = (size_t)len + 1;
+    msg = malloc(size);
+    if (!msg) {
+        va_end(args);
+        return;
+    }
+    vsprintf_s(msg, size, format, args);
+#else
+    if (vasprintf(&msg, format, args) == -1) {
+        msg = NULL;
+    }
+#endif
     va_end(args);
+
+    if (msg) {
+        LogPointer.debug(msg);
+        free(msg);
+    }
 }
 
 void initLogPointer(LogFunction log, LogFunction error, LogFunction debug) {
